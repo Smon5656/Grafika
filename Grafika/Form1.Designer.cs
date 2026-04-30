9a4b75f5bf77a4fe67428d65e88d34deb488ddfe
Šimon Piovarči <simon.piovarci@icloud.com>
2026-04-30 12:51:01 +0200

    Přidejte soubory projektu.

---
+++ Grafika/Form1.Designer.cs
@@ -0,0 +1,80 @@
+﻿namespace Grafika
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Vyžaduje se proměnná návrháře.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Uvolněte všechny používané prostředky.
+        /// </summary>
+        /// <param name="disposing">hodnota true, když by se měl spravovaný prostředek odstranit; jinak false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kód generovaný Návrhářem Windows Form
+
+        /// <summary>
+        /// Metoda vyžadovaná pro podporu Návrháře - neupravovat
+        /// obsah této metody v editoru kódu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.txtDisplay = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.tmrCas = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // txtDisplay
+            // 
+            this.txtDisplay.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.txtDisplay.Location = new System.Drawing.Point(24, 192);
+            this.txtDisplay.Name = "txtDisplay";
+            this.txtDisplay.Size = new System.Drawing.Size(314, 38);
+            this.txtDisplay.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(445, 192);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(185, 38);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // tmrCas
+            // 
+            this.tmrCas.Tick += new System.EventHandler(this.tmrCas_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(686, 242);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtDisplay);
+            this.Name = "Form1";
+            this.Text = "Testy Grafiky";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtDisplay;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Timer tmrCas;
+    }
+}
+
---
+++ Grafika/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Grafika
+{
+    public partial class Form1 : Form
+    {
+        //------------------------------------------------------------------------
+        //kontruktor
+        //------------------------------------------------------------------------
+        public Form1()
+        {
+            InitializeComponent();
+            //start timeru
+            
+            tmrCas.Interval = 1000;
+            tmrCas.Start();
+        }
+
+        //stisk testovaciho tlacitka
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //my si to udelame slozitejsi, abychom si to procvicili
+            //ja si tam mohu dat strint promenna a pak dat, ze se rozna data.time.now.hour,tostrint()
+            //a pak txt.displat.test = lstrCas;
+
+            //string lstrCas;
+            //naplnim promennou casem
+
+            //lstrCas = DateTime.Now.Hour.ToString() + ":"; //krokovani, F9 dam break point a F10 pokracuju
+            //lstrCas += DateTime.Now.Minute.ToString() + ":";
+            //lstrCas += DateTime.Now.Second.ToString();
+
+            //vypisu na display
+            //txtDisplay.Text = lstrCas;
+            
+        }
+
+        private void tmrCas_Tick(object sender, EventArgs e)
+        {
+            string lstrCas;
+            //naplnim promennou casem
+
+            lstrCas = DateTime.Now.Hour.ToString() + ":"; //krokovani, F9 dam break point a F10 pokracuju
+            lstrCas += DateTime.Now.Minute.ToString() + ":";
+            lstrCas += DateTime.Now.Second.ToString();
+
+            //vypisu na display
+            txtDisplay.Text = lstrCas;
+
+            //musim sdelit timeru aby tikal, v kontruktoru
+        }
+    }
+}
